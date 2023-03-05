978f68fb6b6534adb7a45f362c5d76b9893c597b
Kevin Alfonzo <kalfonzoespinett@gmail.com>
2023-03-05 16:14:27 -0400

    client add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+//COMPONENTS
+import Landing from './components/Landing/Landing';
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Dogs</h1>
+      <Landing/>
     </div>
   );
 }
---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,13 @@
+//STYLES
+import Styles from './Landing.module.css'
+
+const Landing = () => {
+    return (
+        <div >
+            <h1>¡Hello Human!</h1>
+            <h4>Search breeds of dogs or create one</h4>
+        </div>
+    )
+}
+
+export default Landing
